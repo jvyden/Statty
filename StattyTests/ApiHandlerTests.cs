c669044115ad20e43a562f30a44bcfd4a968b0a7
jvyden <jvyden@volatile.bz>
2020-12-22 01:05:13 -0500

    [+Unit tests] Add API Handler tests

--- StattyTests/ApiHandlerTests.cs
+++ StattyTests/ApiHandlerTests.cs
@@ -1,11 +1,40 @@
+using System.Threading.Tasks;
 using NUnit.Framework;
-// using StattyBot.
+using StattyBot.structs;
+using StattyBot.util;
 namespace StattyTests {
     [TestFixture]
     public class ApiHandlerTests {
+        private ApiHandler apiHandler = new ApiHandler();
+        
         [Test]
-        public void TestsWorking() {
-            Assert.True(true);
+        [Timeout(5000)]
+        public void CanGetUserFromName() {
+            Task<User> task = apiHandler.UserProfile("Statty");
+            task.ContinueWith(task1 => {
+                User user = task1.Result;
+                
+                Assert.NotNull(user);
+                Assert.True(user.UserId == 1695);
+                Assert.True(user.Username == "Statty");
+                
+                Assert.Pass();
+            });
+        }
+        
+        [Test]
+        [Timeout(5000)]
+        public void CanGetUserFromID() {
+            Task<User> task = apiHandler.UserProfile(1695);
+            task.ContinueWith(task1 => {
+                User user = task1.Result;
+                
+                Assert.NotNull(user);
+                Assert.True(user.UserId == 1695);
+                Assert.True(user.Username == "Statty");
+                
+                Assert.Pass();
+            });
         }
     }
 }
